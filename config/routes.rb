45a81ad9734ccb430eabab71d219113f8bec95b0
Janmarc <janmarc.peconcillo@gmail.com>
2018-01-13 15:20:44 -0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'user/new'
-
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'user#new'
 end
